c02b850f1b21d6e4e9ff538dfcdc96e79ac5339f
Administrator <admin@GL-160.local>
2011-10-27 08:11:02 -0600

    Initial Commit

---
+++ BindMansBluff/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BindMansBluff
+//
+//  Created by Ben Coumerilh on 10/27/11.
+//  Copyright (c) 2011 Northwest Kansas Technical College. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ BindMansBluff/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BindMansBluff
+//
+//  Created by Ben Coumerilh on 10/27/11.
+//  Copyright (c) 2011 Northwest Kansas Technical College. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
